64c316a31478a94ba9f33c85861877ce68f24fc6
Gaeljerez <jerezpapols@gmail.com>
2026-04-06 09:44:19 -0500

    variable edad

--- main.java
+++ main.java
@@ -1,9 +1,21 @@
 import java.util.*;
 
+import javax.swing.JOptionPane;
+
 public class main {
 
     public static void main(String[] args) {
-        
-        System.out.println("holaa");
+        Scanner sc = new Scanner(System.in);
+
+        int edad;
+
+        System.out.println("Ingrese su edad: ");
+        edad =sc.nextInt();
+
+        if(edad >= 18){
+            JOptionPane.showMessageDialog(null, "es mayor de edad");
+        } else {
+            JOptionPane.showMessageDialog(null, "Es menor de edad");
+        }
     }
 }
